183962ec16d99c5040f2efb7f3746a0a5d5df8d5
Votre Nom <tom@p113.io>
2024-04-16 02:45:24 +0200

    iso hunt

--- complete-iso.sh
+++ complete-iso.sh
@@ -39,27 +39,50 @@ PACMAN_CONF_PATH="$ARCHLIVE_PATH/pacman.conf"
 LOCAL_REPO_NAME="x86_64"
 LOCAL_REPO_PATH="$HOME/$LOCAL_REPO_NAME"
 
+# Check if $ARCHLIVE_PATH exists
+if [ -d "$ARCHLIVE_PATH" ]; then
+    # Prompt the user to ask if they want to erase it
+    read -p "The directory $ARCHLIVE_PATH already exists. Do you want to erase it? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            # User wants to erase: remove the directory recursively
+            sudo rm -rf "$ARCHLIVE_PATH"
+            echo "Directory $ARCHLIVE_PATH has been removed."
+            ;;
+        *)
+            # User does not want to erase or response is invalid: exit the script
+            echo "Keeping the existing directory. Exiting script."
+ 
+            ;;
+    esac
+fi
+
+# Create the directory with necessary parents
+mkdir -p "$ARCHLIVE_PATH"
+echo "$ARCHLIVE_PATH directory is created."
+
 # Function: Install missing packages
 install_missing_packages() {
     sudo pacman -Sy --noconfirm --needed "${PACKAGES_TO_INSTALL[@]}"
 }
+
 # Function: Create local repository and add to pacman.conf
 create_local_repository() {
     mkdir -p "$LOCAL_REPO_PATH"
     chown "$USER":"$USER" "$LOCAL_REPO_PATH"
 
-    # Ajouter le dépôt local au pacman.conf de l'archiso
-    if ! grep -q "^\[$local_repo_name\]" "$ARCHLIVE_PATH/pacman.conf"; then
+    # Add the local repository to pacman.conf if not already present
+    if ! grep -q "^\[$LOCAL_REPO_NAME\]" "$PACMAN_CONF_PATH"; then
         echo "Adding local repository to pacman.conf"
-        sudo tee -a "$ARCHLIVE_PATH/pacman.conf" > /dev/null <<EOL
-[$local_repo_name]
+        cat <<EOF | sudo tee -a "$PACMAN_CONF_PATH"
+[$LOCAL_REPO_NAME]
 SigLevel = Optional TrustAll
-Server = file://$local_repo_path
-EOL
+Server = file://$LOCAL_REPO_PATH
+EOF
     fi
 }
 
-# Function: Clean system of unnecessary packages (optional, use with caution!)
+# Function: Clean system of unnecessary packages (use with caution!)
 clean_system() {
     yay -Rns $(yay -Qtdq) --noconfirm 
     yay -Sc --noconfirm
@@ -76,8 +99,6 @@ copy_config_files() {
 
     # Extraction des icônes dans /usr
     sudo tar -xzf "$ARCHISO_PATH/calam-archives/usr_icons.tar.gz" -C "$ARCHLIVE_PATH/airootfs/usr"
-
-
 }
 
 # Function: Backup configurations (customize paths as needed)
@@ -155,8 +176,8 @@ backup_configs() {
 
    
     # Add the packages list to the custom archiso
-    yay -Qqe > "$ARCHLIVE_PATH/packages.x86_64"
-    cp "$ARCHLIVE_PATH/packages.x86_64" "$ARCHLIVE_PATH/$LOCAL_REPO_NAME/airootfs/"
+    yay -Qqe > $ARCHLIVE_PATH/$LOCAL_REPO_NAME/airootfs/packages.x86_64"
+    # cp "$ARCHLIVE_PATH/packages.x86_64" "$ARCHLIVE_PATH/$LOCAL_REPO_NAME/airootfs/"
 
     # Add code here to backup etc and user configs...
     sudo tar -czvf "$ARCHLIVE_PATH/etc_backup.tar.gz" -C /etc/ --exclude-caches-all --exclude-vcs
@@ -201,248 +222,3 @@ mkdir -p $ARCHISO_OUT
 # Step 6: Build ISO (make sure to customize mkarchiso command according to your needs)
 sudo mkarchiso -v -w "$ARCHLIVE_PATH" -o "$ARCHISO_OUT" "$ARCHLIVE_PATH"
 
-
-# Define the path to the profile's package list
-# package_list="$ARCHLIVE_PATH/packages.x86_64"
-# # Packages to check for
-# packages=("syslinux" "memtest86+" "memtest86+-efi")
-
-
-# # Function to check if a given package is in the package list
-# check_package() {
-#     local package=$1
-#     if grep -Fxq "$package" "$package_list"; then
-#         echo -e "\e[32mFOUND: $package\e[0m"
-#     else
-#         echo -e "\e[31mMISSING: $package\e[0m"
-#     fi
-# }
-
-# # Check if each package is present in the package list
-# echo "Checking for required packages in the package list..."
-# for package in "${packages[@]}"; do
-#     check_package "$package"
-# done
-
-# # unzip and copy calamares files in archlive 
-# tar -xzvf  $ARCHISO_PATH/calam/etc-calamares.tar.gz $ARCHLIVE_PATH/airootfs/etc
-# tar -xzvf  $ARCHISO_PATH/calam-archives/usr_calamares.tar.gz $ARCHLIVE_PATH/airootfs/usr
-# tar -xzvf  $ARCHISO_PATH/calam-archives/usr_icons.tar.gz $ARCHLIVE_PATH/airootfs/usr
-
-# find and replace in every files of the $ARCHLIVE_PATH parch by arch 
-#find "$ARCHLIVE_PATH" -type f -exec sed -i 's/parch/arch/g' {} +
-
-# sudo cp /etc/pacman.conf $ARCHLIVE_PATH/pacman.conf
-
-# declare -a packages=(
-#     "adw-gtk3-git"
-#     "adwaita-qt5-git"
-#     "adwaita-qt6-git"
-#     "anyrun-git"
-#     "blackbox-terminal"
-#     "brave-bin"
-#     "calamares"
-#     "extension-manager"
-#     "glpaper-hg"
-#     "gnome-arch"
-#     "gojq"
-#     "gradience-git"
-#     "hack-nerd-fonts"
-#     "hypridle-git"
-#     "hyprland-git"
-#     "hyprlock-git"
-#     "hyprpicker-git"
-#     "hyprshade"
-#     "hyprshot"
-#     "jan-bin"
-#     "neofetch-git"
-#     "nwg-look-bin"
-#     "arch-branding"
-#     "arch-emoji-ios"
-#     "arch-gdm-config"
-#     "arch-gnome-backgrounds"
-#     "arch-grub"
-#     "arch-pacman"
-#     "arch-pantheon"
-#     "arch-plymouth"
-#     "archlinux-quickstart"
-#     "paru"
-#     "python-materialyoucolor-git"
-#     "qmplay2"
-#     "swww"
-#     "text-engine"
-#     "ttf-material-symbols-variable-git"
-#     "ttf-readex-pro"
-#     "typora"
-#     "vazirmatn-fonts"
-#     "wlogout"
-#     "xdg-desktop-portal-hyprland-git"
-#     "yay"
-# )
-
-# # Définir le chemin de ton répertoire local pour archiso
-# local_repo_path="$ARCHLIVE_PATH/airootfs/root/x86_64"
-
-# # Création du répertoire s'il n'existe pas
-# sudo mkdir -p $local_repo_path
-# sudo chown -R $USER:$USER $local_repo_path
-# # Log file pour suivre le progrès
-# progress_log="$ARCHLIVE_PATH/install_progress.log"
-
-# # Vérifier où l'installation s'est arrêtée la dernière fois
-# if [[ -f "$progress_log" ]]; then
-#     readarray -t installed_packages < "$progress_log"
-# else
-#     installed_packages=()
-# fi
-
-# # Vérifier les packages déjà téléchargés dans le répertoire local_repo_path et marquer comme installés
-# for package in "${packages[@]}"; do
-#     tarball_name=$(find "$local_repo_path" -name "${package}*.pkg.tar.gz")
-#     if [[ -n $tarball_name ]]; then
-#         # Package déjà téléchargé, le marquer comme installé
-#         if ! [[ " ${installed_packages[*]} " =~ " ${package} " ]]; then
-#             installed_packages+=("$package")
-#             echo "$package" >> "$progress_log"
-#         fi
-#     fi
-# done
-
-# # Fonction pour vérifier si un paquet a déjà été installé
-# # Function to check if a package is already installed in the local repo
-# package_installed() {
-#     grep -Fxq "$1" "$progress_log" && return 0 || return 1
-# }
-
-# # Ensure local repo directory exists
-# mkdir -p "$local_repo_path"
-
-# # Ensure progress log file exists
-# touch "$progress_log"
-
-# # Loop to clone, build, and copy each package
-# for pkg in "${packages[@]}"; do
-#     if package_installed "$pkg"; then
-#         echo "Le paquet $pkg est déjà installé. Passage au suivant."
-#         continue
-#     fi
-    
-#     echo "Traitement du paquet: $pkg"
-    
-#     # Clone the AUR repository
-#     if ! git clone "https://aur.archlinux.org/$pkg.git"; then
-#         echo "Erreur lors du clonage de $pkg"
-#         exit 1
-#     fi
-
-#     # Move into the repository directory
-#     cd "$pkg" || exit
-
-#     # Build the package
-#     if ! makepkg -sci --noconfirm; then
-#         echo "Échec de la construction du paquet $pkg"
-#         exit 1
-#     fi
-    
-#     # Copy the package files to the local repository path
-#     if ! cp *.pkg.tar.zst "$local_repo_path"; then
-#         echo "Échec de la copie du paquet $pkg vers $local_repo_path"
-#         exit 1
-#     fi
-#     # Add it to your local repository
-#     repo-add "$local_repo/$pkg.db.tar.gz" *.pkg.tar.zst
-
-#     # Cleanup
-#     cd .. || exit
-#     rm -rf "$pkg"
-
-#    # Add the package to the list of installed packages in the progress log
-#     echo "$pkg" >> "$progress_log"
-
-#     echo "Le paquet $pkg a été ajouté au répertoire local avec succès."
-# done
-
-# echo "Tous les paquets ont été traités et ajoutés au répertoire local."
-# Define local repository details
-# local_repo_name='x86_64'
-
-# # Define the path to the archiso profile's pacman.conf
-# ARCHISO_PACMAN_CONF="$ARCHLIVE_PATH/pacman.conf"
-
-# # Function to extract and append repositories to archiso pacman.conf
-# append_repositories() {
-#     # Use a temporary file to store new entries before appending them
-#     local temp_conf="$(mktemp)"
-    
-#     # Read main pacman.conf and included files within /etc/pacman.d/* (if they exist)
-#     for file in /etc/pacman.conf /etc/pacman.d/*; do
-#         if [[ -f $file ]]; then
-#             while IFS= read -r line || [[ -n "$line" ]]; do
-#                 # Check if the line contains a repository definition
-#                 if [[ "$line" =~ ^\[* ]]; then
-#                     # Extracting repository name
-#                     repo_name=$(echo $line | sed 's/\[\(.*\)\]/\1/')
-#                     # Avoid adding duplicate entries
-#                     if ! grep -q "^\[$repo_name\]" "$ARCHISO_PACMAN_CONF"; then
-#                         # Append the repository name to the temp_conf
-#                         echo "$line" >> "$temp_conf"
-#                         # Read the next line for the server or include directive
-#                         read -r next_line
-#                         echo "$next_line" >> "$temp_conf"
-#                     fi
-#                 fi
-#             done < "$file"
-#         fi
-#     done
-    
-#     if [ -s "$temp_conf" ]; then
-#         # If the temp_conf file has content, append it to ARCHISO_PACMAN_CONF with sudo
-#         sudo bash -c "cat '$temp_conf' >> '$ARCHISO_PACMAN_CONF'"
-#     fi
-
-#     # Remove the temporary file
-#     rm "$temp_conf"
-# }
-
-# # Backup the original archiso pacman.conf file using sudo
-# sudo cp "$ARCHISO_PACMAN_CONF" "${ARCHISO_PACMAN_CONF}.bak"
-
-# # Call the function to append repositories
-# append_repositories
-
-# echo "Repositories have been appended to your archiso pacman.conf."
-
-# # Add local repository to pacman.conf if it's not already there
-# if ! grep -q "^\[$local_repo_name\]" "$ARCHLIVE_PATH/pacman.conf"; then
-#     echo "Adding local repository to pacman.conf"
-#     sudo tee -a "$ARCHLIVE_PATH/pacman.conf" > /dev/null <<EOL
-
-# [$local_repo_name]
-# SigLevel = Optional TrustAll
-# Server = file://$local_repo_path
-# EOL
-# fi
-
-# # Check if mkarchiso is currently running
-# if pgrep -x "mkarchiso" > /dev/null; then
-#     echo -e "\e[33mThe 'mkarchiso' process is currently running...\e[0m"
-# else
-#     echo "The 'mkarchiso' process is not currently running."
-
-#     # Optionally, check for a successful build by examining the last modified ISO file
-#     iso_files=($(find $ARCHISO_OUT -maxdepth 1 -name "*.iso" -type f -printf '%T+ %p\n' | sort -r))
-#     if [ "${#iso_files[@]}" -gt 0 ]; then
-#         # The most recently modified ISO file will be at the top
-#         latest_iso=${iso_files[0]##* }
-#         echo -e "\e[32mThe latest ISO built was on ${latest_iso}\e[0m"
-#     else
-#         echo -e "\e[31mNo ISO files found in output directory.\e[0m"
-#         # Build the ISO
-#         # Make sure both directories exist or mkarchiso will give an error
-#         mkdir -p "$ARCHLIVE_PATH"
-#         mkdir -p "$ARCHISO_OUT"
-#         sudo mkarchiso -v -w "$ARCHLIVE_PATH" -o "$ARCHISO_OUT" "$ARCHLIVE_PATH"
-
-#         exit 1
-#     fi
-# fi
